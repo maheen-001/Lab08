d8155fa8a7f565e2a43a7bea9f7592e564139cb0
maheen-001 <maabbas1@ualberta.ca>
2025-10-28 09:10:26 -0600

    Create failing tests for hasCity (red phase)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList customList = new CustomList();
+        City city = new City("Edmonton", "Alberta");
+        customList.addCity(city);
+
+        // should be true for a city that was added
+        assertTrue(customList.hasCity(city));
+
+        // should be false for a city that wasn't added
+        City fakeCity = new City("Calgary", "Alberta");
+        assertFalse(customList.hasCity(fakeCity));
+    }
+
 }
